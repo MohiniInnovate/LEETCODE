7049b9f9a19260a785374d1bbd5ad202267224de
MohiniInnovate <mohinibatra1999@gmail.com>
2026-02-27 02:10:25 +0530

    Time: 27 ms (77.08%) | Memory: 53.9 MB (8.31%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        stack = []
+        curr = head
+
+        while curr:
+            stack.append(curr.val)
+            curr = curr.next
+        if stack == stack[::-1]:
+            return True
+        return False
