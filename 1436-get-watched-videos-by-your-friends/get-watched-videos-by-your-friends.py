278f4289bf9ff8eac7ae48396731960ad08fcd5c
MohiniInnovate <mohinibatra1999@gmail.com>
2026-03-06 12:50:07 +0530

    Time: 23 ms (37.95%) | Memory: 21.2 MB (37.78%) - LeetSync

---
+++ 1436-get-watched-videos-by-your-friends/get-watched-videos-by-your-friends.py
@@ -0,0 +1,41 @@
+class Solution:
+    def watchedVideosByFriends(self, watchedVideos: List[List[str]], friends: List[List[int]], id: int, level: int) -> List[str]:
+        f = defaultdict(list)
+        
+        q = deque()
+
+        visit = set()
+
+        q.append((id, 0))
+
+
+        ans = []
+        visit.add(id)
+        freq = {}
+
+        while q:
+            fr, d = q.popleft()
+
+            if d == level:
+                ans.append(fr)
+                continue
+        
+            for a in friends[fr]:
+                if a not in visit:
+                    q.append((a, d + 1 ))
+                    visit.add(a)
+        while ans:
+            n = ans.pop()
+            for c in watchedVideos[n]:
+                if c in freq:
+                    freq[c] += 1
+                else:
+                    freq[c] = 1
+        sorted_ans = dict(sorted(freq.items(), key=lambda item: (item[1], item[0])))
+        return list(sorted_ans.keys())
+        
+        
+
+
+        
+        
