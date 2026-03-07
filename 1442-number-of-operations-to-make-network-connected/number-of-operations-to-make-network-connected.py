1107143136c48bb1dcfd8a763788cc84e466a3ae
MohiniInnovate <mohinibatra1999@gmail.com>
2026-03-08 00:39:12 +0530

    Time: 23 ms (98.90%) | Memory: 32.6 MB (98.53%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
@@ -0,0 +1,38 @@
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        par = [i for i in range(n)]
+        size = [1] * (n)
+
+        def find(c):
+            p = par[c]
+
+            while p != par[p]:
+                p = par[p]
+            return p
+        
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+
+            if p1 != p2:
+                if size[p1] > size[p2]:
+                    par[p2] = p1
+                    size[p1] += size[p2]
+                else:
+                    par[p1] = p2
+                    size[p2] += size[p1]
+
+        nodes = set()
+
+        for a,b in connections:
+            union(a,b)
+        
+        comp = sum(1 for i in range(n) if i == par[i])
+        connected = n - comp + 1
+        wires_used = connected - 1
+        wires_left = len(connections) - wires_used
+        wires_needed = comp - 1
+        if wires_left >= wires_needed:
+            return wires_needed
+        return -1 
+
+
