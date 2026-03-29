1aeae9324e102f503d2b5b09f9226b544dbf4863
MohiniInnovate <mohinibatra1999@gmail.com>
2026-03-30 00:23:00 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (90.64%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.py
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.py
@@ -1,6 +1,21 @@
 class Solution:
     def canBeEqual(self, s1: str, s2: str) -> bool:
-        def equal(s, i):
+        a = sorted([s1[0], s1[2]])
+        b = sorted([s1[1], s1[3]])
+        c = sorted([s2[0], s2[2]])
+        d = sorted([s2[1], s2[3]])
+
+        return a == c and b == d
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        '''def equal(s, i):
             if s == s2:
                 return True
             if i == 2:
@@ -21,4 +36,4 @@ class Solution:
                 return True
             
             return False
-        return equal(s1, 0)
+        return equal(s1, 0)'''
