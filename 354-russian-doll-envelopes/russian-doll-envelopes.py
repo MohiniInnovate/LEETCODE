2ce7a04051a1c1167b3a5a376c3eb82fb318283d
MohiniInnovate <mohinibatra1999@gmail.com>
2026-04-07 02:22:00 +0530

    Time: 225 ms (23.63%) | Memory: 55.2 MB (23.31%) - LeetSync

---
+++ 354-russian-doll-envelopes/russian-doll-envelopes.py
@@ -0,0 +1,38 @@
+class Solution:
+    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+        envelopes.sort(key = lambda x: (x[0],-x[1]))
+        res = []
+        
+
+        for e in envelopes:
+            if not res:
+                res.append(tuple(e))
+            else:
+                w1, h1 = res[-1]
+                w2, h2 = e
+                if w1 < w2 and h1 < h2 : 
+                    res.append(tuple(e))
+                else:
+                    i1 = bisect_left(res, w2, key=lambda x: x[0])
+                    i2 = bisect_left(res, h2, key=lambda x: x[1])
+                    i = min(i1,i2)
+                    res[i] = tuple(e)
+        return len(res)
+
+
+        '''
+        n = len(envelopes)
+        dp = [1] * n
+        ans = 0
+
+        for i in range(n-1, -1, -1):
+            w1, h1 = envelopes[i]
+            res = 0
+            for j in range(i+1, n):
+                w2, h2 = envelopes[j]
+                if w1 < w2 and h1 < h2 :
+                    res = max(res, dp[j])
+            dp[i] += res
+            ans = max(ans, dp[i])
+        return ans'''
+        
