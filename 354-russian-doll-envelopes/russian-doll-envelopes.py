a8da8e896916b12134adfe371befe12dbed7c56a
MohiniInnovate <mohinibatra1999@gmail.com>
2026-04-08 02:01:03 +0530

    Time: 107 ms (91.32%) | Memory: 54.9 MB (49.84%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.py
+++ 354-russian-doll-envelopes/russian-doll-envelopes.py
@@ -4,33 +4,11 @@ class Solution:
         res = []
         
 
-        for e in envelopes:
-            if not res:
-                res.append(tuple(e))
+        for w, h in envelopes:
+            i = bisect_left(res,h)
+            if i == len(res):
+                res.append(h)
             else:
-                w1, h1 = res[-1]
-                w2, h2 = e
-                if w1 < w2 and h1 < h2 : 
-                    res.append(tuple(e))
-                else:
-                    i = bisect_left(res, h2, key=lambda x: x[1])
-                    res[i] = tuple(e)
+                res[i] = h
         return len(res)
-
-
-        '''
-        n = len(envelopes)
-        dp = [1] * n
-        ans = 0
-
-        for i in range(n-1, -1, -1):
-            w1, h1 = envelopes[i]
-            res = 0
-            for j in range(i+1, n):
-                w2, h2 = envelopes[j]
-                if w1 < w2 and h1 < h2 :
-                    res = max(res, dp[j])
-            dp[i] += res
-            ans = max(ans, dp[i])
-        return ans'''
-        
+            
