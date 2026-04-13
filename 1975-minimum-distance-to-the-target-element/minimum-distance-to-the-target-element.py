74d86d75b08be4b1ba21e6827b7db85ba85f372c
MohiniInnovate <mohinibatra1999@gmail.com>
2026-04-14 01:57:20 +0530

    Time: 1 ms (25.22%) | Memory: 19.1 MB (99.41%) - LeetSync

---
+++ 1975-minimum-distance-to-the-target-element/minimum-distance-to-the-target-element.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
+        ans = float('inf')
+
+        for i,n in enumerate(nums):
+            if n == target:
+                ans = min(ans, abs(i-start))
+        return ans 
