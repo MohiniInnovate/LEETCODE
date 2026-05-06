7a357e9f378bf83d8569a7084257a8460fd081ed
MohiniInnovate <mohinibatra1999@gmail.com>
2026-05-06 23:41:49 +0530

    Time: 164 ms (35.65%) | Memory: 74.1 MB (26.82%) - LeetSync

---
+++ 494-target-sum/target-sum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        dp = {}
+        
+        def dfs(i, total):
+
+            if i == n:
+                if total == target:
+                    return 1
+                return 0
+
+            if (i,total) in dp:
+                return dp[(i,total)]
+
+            dp[(i,total)] = dfs(i+1, total + nums[i]) + dfs(i+1, total - nums[i])
+            
+            return dp[(i,total)]
+
+        
+        return dfs(0,0)
+            
